643d6e651218fa5a7a547ca0a46695038355415a
Pankaj Jha <pankajjhawork@gmail.com>
2024-11-15 15:26:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function firstWord(s) {
   // your code here
-	let str=s.trim().split("")
-	return str[0]
+	let arr=s.trim().split("")
+	return arr[0]
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
